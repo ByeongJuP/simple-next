9d63c4a5caac8100fc8c9bdd6ba14bbae94a232d
byeongJuP <pbju92@gmail.com>
2022-09-16 17:53:21 +0900

    tmp commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,33 @@
+import {
+  AppBar,
+  Button,
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import MenuIcon from "@material-ui/icons";
+import { Fragment, useState } from "react";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const [anchor, setAnchor] = useState(null);
+
+  const closeMenu = () => setAnchor(null);
+
+  return (
+    <>
+      <AppBar>
+        <Toolbar>
+          <Typography variant="h6" color="inherit">
+            nextjs page
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default MyApp;
